10188100728639ef9044585a5145e10604010d7f
ArnauldLemaire <albator1975@hotmail.com>
2024-06-18 19:51:28 +0200

    Version initiale

---
+++ ProgrammeAge.py
@@ -0,0 +1,6 @@
+# Demande à l'utilisateur de saisir son nom et son âge
+nom = input("Quel est votre nom ? ")
+age = input("Quel est votre âge ? ")
+
+# Affiche un message de bienvenue personnalisé
+print(f"Bonjour {nom}, vous avez {age} ans.")
