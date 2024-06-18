01c8f9046005aff442747f17e04925fa55284159
ArnauldLemaire <albator1975@hotmail.com>
2024-06-18 20:06:41 +0200

    Ajout d'un texte suivant l'age

--- ProgrammeAge.py
+++ ProgrammeAge.py
@@ -4,3 +4,13 @@ age = input("Quel est votre âge ? ")
 
 # Affiche un message de bienvenue personnalisé
 print(f"Bonjour {nom}, vous avez {age} ans.")
+
+if int(age) < 40:
+    print(f"Vous êtes encore jeune")
+elif int(age) < 50:
+    print(f"Bientôt vieux !")
+elif int(age) < 60:
+    print(f"Vieux ...")
+elif int(age) > 60:
+    print(f"La retraite approche")
+
